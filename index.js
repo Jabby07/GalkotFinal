08c0cfc48dbe648cb637ba9a4601fed64524e14e
Jabby07 <134323420+Jabby07@users.noreply.github.com>
2024-12-16 05:25:43 -0300

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const jsonServer = require ("json-server");
+const cors = require("cors");
 const server =jsonServer.create();
 const router = jsonServer.router("almacen.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 10000;
 
+
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+    console.log(`JSON Server está corriendo en el puerto ${port}`);
+});
